dd2e9901dcc6afbb51228eb5cf76ce1d0a5f5c64
Riyadh Uddin <riyadhuddin95@gmail.com>
2022-01-25 04:55:48 +0600

    Delete analysis101.py

--- analysis101.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-import numpy as np 
-import streamlit as st
-data = pd.read_csv('data/PakistanLargestEcommerceDataset.csv')
-st.write("### PakistanLargestEcommerceDataset", data.head(5))
